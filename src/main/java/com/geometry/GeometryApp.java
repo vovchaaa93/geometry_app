4c53d5ecbeffcbe1237c9ff36e246211d816d89d
Владимир <57352868+vovchaaa93@users.noreply.github.com>
2025-09-16 05:12:36 -0700

    use GeometryUtils

--- src/main/java/com/geometry/GeometryApp.java
+++ src/main/java/com/geometry/GeometryApp.java
@@ -4,6 +4,7 @@ import com.geometry.shapes.Circle;
 import com.geometry.shapes.Rectangle;
 import com.geometry.shapes.Shape;
 import com.geometry.shapes.Triangle;
+import com.geometry.utils.GeometryUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,5 +19,10 @@ public class GeometryApp {
         for (Shape shape : shapeList) {
             System.out.println(shape.getInfo());
         }
+
+        System.out.println(GeometryUtils.cmToMeters(125));
+        System.out.println(GeometryUtils.compareAreas(shapeList.get(0), shapeList.get(1)));
+        System.out.println(GeometryUtils.getLargestShape(shapeList.get(1), shapeList.get(2)));
+
     }
 }
