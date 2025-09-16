e680b00bb1dbd77eec6e814370c79e236c5d8690
Владимир <57352868+vovchaaa93@users.noreply.github.com>
2025-09-16 05:38:53 -0700

    GeometryApp with 3D Shapes

--- src/main/java/com/geometry/GeometryApp.java
+++ src/main/java/com/geometry/GeometryApp.java
@@ -4,6 +4,9 @@ import com.geometry.shapes.Circle;
 import com.geometry.shapes.Rectangle;
 import com.geometry.shapes.Shape;
 import com.geometry.shapes.Triangle;
+import com.geometry.shapes.threeD.Cube;
+import com.geometry.shapes.threeD.Shape3D;
+import com.geometry.shapes.threeD.Sphere;
 import com.geometry.utils.GeometryUtils;
 
 import java.util.ArrayList;
@@ -24,5 +27,12 @@ public class GeometryApp {
         System.out.println(GeometryUtils.compareAreas(shapeList.get(0), shapeList.get(1)));
         System.out.println(GeometryUtils.getLargestShape(shapeList.get(1), shapeList.get(2)));
 
+        List<Shape3D> shape3DList = new ArrayList<>();
+        shape3DList.add(new Cube(2));
+        shape3DList.add(new Sphere(5));
+
+        for (Shape3D shape3D : shape3DList) {
+            System.out.println(shape3D.getInfo());
+        }
     }
 }
